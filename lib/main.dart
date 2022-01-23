6dc3ee620ad7369de7efb964fa592de0556ec621
hanifiamdev <hanifamrullahdev@gmail.com>
2022-01-23 21:42:27 +0700

    Add title in App Bar

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(title: Text("My Apps")),
       ),
     );
   }
